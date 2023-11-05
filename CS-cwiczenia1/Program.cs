74e4bce4bf0ecd331b923be69ad284a15b508e06
SzaliKapitana <rafal.szala03@gmail.com>
2023-11-05 08:42:03 +0100

    mój pierwszy program

--- CS-cwiczenia1/Program.cs
+++ CS-cwiczenia1/Program.cs
@@ -6,7 +6,8 @@ namespace CS_cwiczenia1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World " +
+                "   ");
         }
     }
 }
