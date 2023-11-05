a11063783862acc8295fba1c8c187e1c63b273f7
SzaliKapitana <rafal.szala03@gmail.com>
2023-11-05 08:28:33 +0100

    Dodaj pliki projektów.

---
+++ CS-cwiczenia1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CS_cwiczenia1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
